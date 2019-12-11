200e7bf1e7e6630154e1ee161749fca53edbf968
Nagarajan Subramanian <42885137+NagarajanSubramanian@users.noreply.github.com>
2019-12-12 00:40:40 +0530

    Revert "refs #commit-branch login, signup page added"

--- src/main/java/com/springboot/boot/dao/UserTableDao.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.boot.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.springboot.boot.entity.UserTable;
-
-public interface UserTableDao extends CrudRepository<UserTable, String> {
-
-}
